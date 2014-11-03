1732559d0df5f9be06846db224d894dc467f1b7d
David Fisher <tibbon@gmail.com>
2014-11-03 16:27:45 -0500

    Removed --warnings from .rspec file and setup spec names properly

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it "has a valid factory" do
+RSpec.describe User, :type => :model do
+    it "has a valid factory" do
     expect(Factory.create(:user)).to be_valid
   end
   it "is invalid without a name"
