99dbb984a356f5cb779ee9a7105c31815ca0dadd
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-03 16:23:53 -0500

    removed migrations, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :threads do
-    resources :comments
+  resources :users, except: [:new, :edit]
+  resources :threads, except: [:new, :edit] do
+    resources :comments, except: [:new, :edit]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/migrate/20141103193627_create_votes.rb
+++
@@ -1,9 +0,0 @@
-class CreateVotes < ActiveRecord::Migration
-  def change
-    create_table :votes do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :comment, index: true
-      t.belongs_to :thread, index: true
-    end
-  end
-end
