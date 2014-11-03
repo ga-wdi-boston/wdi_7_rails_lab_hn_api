b90863ec08354e05acb76f32432035fdd48e90a5
David Fisher <tibbon@gmail.com>
2014-11-03 16:14:08 -0500

    Updated submission model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,8 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :votes
+
+  def self.top_20
+    Post.all.limit(20) # Not so great, but works for a moment
+  end
 end
