7e8be2da6ab91e232489fdce8dc4165d26d3c6b7
raielin@gmail.com <raielin@gmail.com>
2014-11-03 17:25:19 -0500

    Created unique indices for votes where submission and user, and comment and user, must be unique.

--- db/migrate/20141103222248_create_votes.rb
+++ db/migrate/20141103222248_create_votes.rb
@@ -7,4 +7,7 @@ class CreateVotes < ActiveRecord::Migration
       t.boolean :value
     end
   end
+
+  add_index :votes, [:user_id, :submission_id], unique: true
+  add_index :votes, [:user_id, :comment_id], unique: true
 end
