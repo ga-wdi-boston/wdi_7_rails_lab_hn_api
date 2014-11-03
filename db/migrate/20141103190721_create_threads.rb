b6f8d90e2f2a6a5bd1c5f558a3684282b944457c
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-03 14:08:07 -0500

    generated createthreads migration

---
+++ db/migrate/20141103190721_create_threads.rb
@@ -0,0 +1,11 @@
+class CreateThreads < ActiveRecord::Migration
+  def change
+    create_table :threads do |t|
+      t.string :title
+      t.string :url
+      t.text :content
+      t.integer :points
+      t.belongs_to :user, index: true
+    end
+  end
+end
