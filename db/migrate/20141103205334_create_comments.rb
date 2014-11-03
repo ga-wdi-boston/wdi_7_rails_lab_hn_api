1f567a0b6ccf8cc4ff211201cd8a1e0b7b1d4d4f
shaynaCummings <shayna.cummings@gmail.com>
2014-11-03 15:53:57 -0500

    comments migration

---
+++ db/migrate/20141103205334_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.textuser :content
+      t.belongs_to :submission, index: true
+      t.belongs_to :comment, index: true
+    end
+  end
+end
