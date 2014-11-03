ed0ef11348935db0c9ae782b5b74bcd1e2e581ba
David Fisher <tibbon@gmail.com>
2014-11-03 16:37:41 -0500

    Passing Rspec tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do
     name Faker::Name.name
-    email Faker::Name.email
+    email Faker::Internet.email
     about Faker::Lorem.paragraph(2)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-    it "has a valid factory" do
-    expect(Factory.create(:user)).to be_valid
+RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    expect(FactoryGirl.build_stubbed(:user)).to be_valid
   end
-  it "is invalid without a name"
-  it "is invalid without a email"
+
+  it "is invalid without a name" do
+    user = FactoryGirl.build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).not_to include("can't be blank")
+  end
+
+  it "is invalid without a email" do
+    user = FactoryGirl.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).not_to include("can't be blank")
+  end
 end
