8e51f5aee814148cd4369edab6776bda21d6c3a8
George <gbanis@gmail.com>
2014-11-03 16:02:28 -0500

    Added model relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+  belongs_to :user_id
+  belongs_to :comment
+  belongst_to :comments
+  has_many :comments
+  has_many :votes
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,9 @@
 class Submission < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
+  has_many :votes
+
+  def self.top_20
+
+  end
 end
