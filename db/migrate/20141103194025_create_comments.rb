2467a47148c3d0610a7e7e8d12e7e61745a1f2f2
CollinBlinder <collinblinder@gmail.com>
2014-11-03 15:05:57 -0500

    Made migrations and models

---
+++ db/migrate/20141103194025_create_comments.rb
@@ -0,0 +1,14 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+
+      t.string :content
+
+      t.parent :parent
+      t.belongs_to :post
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20141103194050_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+
+      t.integer :count
+
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
