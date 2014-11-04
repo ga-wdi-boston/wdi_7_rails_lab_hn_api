531985c80441ece33d219aab57d1454840968d88
raielin@gmail.com <raielin@gmail.com>
2014-11-03 20:03:55 -0500

    Corrected syntax in first user model test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it "has a valid factory" do
-    expect(Factory.create(:user)).to be_valid
+    expect(FactoryGirl.build_stubbed(:user)).to be_valid
   end
+
   it "is invalid without a name"
   it "is invalid without an email"
 end
