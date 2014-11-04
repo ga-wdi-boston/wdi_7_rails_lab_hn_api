c39009b035cd0f6e2063fa6c144c299ebaf92ae3
raielin@gmail.com <raielin@gmail.com>
2014-11-03 21:55:57 -0500

    Corrected syntax and name of model

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe User, :type => :model do
+RSpec.describe Submission, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.build_stubbed(:submission)).to be_valid
   end
