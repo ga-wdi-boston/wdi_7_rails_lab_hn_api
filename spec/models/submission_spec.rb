a284b1c7057f1fc17ef666546565bdffad778966
raielin@gmail.com <raielin@gmail.com>
2014-11-03 22:12:23 -0500

    Wrote test for valid title in submission model

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Submission, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.build_stubbed(:submission)).to be_valid
   end
+
+  it "is invalid without a title" do
+    submission = FactoryGirl.build(:submission, title: nil)
+    submission.valid?
+    expect(submission.errors[:title]).not_to include("can't be blank")
+  end
 end
