0fac3797a8d5634981c4e0f3c1c02154adc3f5ac
raielin@gmail.com <raielin@gmail.com>
2014-11-03 19:46:52 -0500

    Corrected require rails_helper in user model spec. Corrected syntax for describe type model test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, :type => :model do
   it "has a valid factory" do
     expect(Factory.create(:user)).to be_valid
   end
