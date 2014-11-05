421a1ebad492eba7f3e53501ff1dc04ce5e868f5
George <gbanis@gmail.com>
2014-11-04 23:23:16 -0500

    Create controller works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,13 @@ class CommentsController < ApplicationController
     render json: @comments
   end
 
+  def create
+    @comment = Comment.create(comment_params)
+    render json: @comment
+  end
+
+  def comment_params
+    params.permit(:content, :user_id, :submission_id)
+  end
 
 end
