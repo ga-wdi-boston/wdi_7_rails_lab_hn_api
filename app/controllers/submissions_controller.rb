e1b2ff95a6efef6ec44a27664e495a626fd1b74d
fishermanswharff <fishermanswharff@github.com>
2014-11-04 19:55:04 -0500

    create method on submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -13,4 +13,18 @@ class SubmissionsController < ApplicationController
     @submissions = Submission.last(20)
     render json: @submissions
   end
+
+  def create
+    @submission = Submission.new(submission_params)
+    if @submission.save
+      render json: @submission, status: :created, location: @submission
+    else
+      render json: @submission.errors, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def submission_params
+    params.require(:submission).permit(:title, :url, :body)
+  end
 end
