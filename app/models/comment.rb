8c20ea685577fee579b9199a4f26f0127bf3b84b
Rebecca Paz <r.pazyaquian@gmail.com>
2014-11-05 11:09:23 -0500

    model specs done, all green

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   belongs_to :comment
+  validates :content, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   belongs_to :comment
+  validates_inclusion_of :value, :in => [true, false]
 end
