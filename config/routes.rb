10d6e81e2fcf0cb152e24aa0e50c23a1037549cf
Lauren Fazah <laurenfazah@gmail.com>
2014-11-05 11:25:11 -0500

    editing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, except: [:new, :edit]
-  resources :posts, except: [:new, :edit]
+  resources :users, except: [:new, :edit] do
+    resources :posts, except: [:new, :edit]
+  end
 
+  resources :posts, except: [:new, :edit]
   resources :comments, only: [:index, :show]
   resources :votes
 end
