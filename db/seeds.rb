7f070825cea3fb5c1189e2cc49b4c241d6402539
George <gbanis@gmail.com>
2014-11-04 21:51:33 -0500

    Added submission seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user_1 = User.create({name: 'George', about: 'An about message', email: 'gbanis@gmail.com'})
 user_2 = User.create({"name"=>"George", "about"=>"An about message", "email"=>"gg@gg.com"})
+
+submission_1 = Submission.create({
+  title: "Google Inbox Given Small Visual Update",
+  url: "http://www.xda-developers.com/android/google-inbox-visual-update/",
+  user_id: 1
+})
+
+submission_2 = Submission.create({
+  title: "Google Inbox Given Small Visual Update",
+  body: "Not too long ago, Google launched an invite-only beta program for its new Email client, Inbox by Gmail. This new service has already gotten to be quite popular in just a few weeks, and many of us are seeing a stream of invitation requests on our G+ and Facebook news feeds. And now that users are routinely receiving three invites every few days, the community is ever expanding.",
+  user_id: 1
+})
+
