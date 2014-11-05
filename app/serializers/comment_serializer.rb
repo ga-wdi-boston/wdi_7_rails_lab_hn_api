6a262de189fbcd815f58cb6b9def64c00ec152d2
raielin@gmail.com <raielin@gmail.com>
2014-11-05 10:11:36 -0500

    Created serializers folder. Created comment serializer.

---
+++ app/serializers/comment_serializer.rb
@@ -0,0 +1,12 @@
+class CommentSerializer < ActiveModel::Serializer
+  attributes :content, :submission_id
+
+  belongs_to :user
+  belongs_to :comment
+  belongs_to :submission
+
+  has_many :votes
+  has_many :comments
+
+  url [:user, :comment, :submission]
+end
