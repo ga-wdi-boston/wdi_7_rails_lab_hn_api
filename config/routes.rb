b89732835a80ac014744c8938d1d48242a75e280
Holly Moody <hollymoody@charter.net>
2014-11-05 10:55:39 -0500

    Update routes, add in rails-cors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :index]
-  resources :submissions, only: [:show, :index]
-  resources :comments, only: [:show, :index]
-  resources :votes, only: [:show, :index]
+  resources :users
+  resources :submissions
+  resources :comments
+  resources :votes
 end
