5fde5888ab0c34c39d4792a319b426f28c06a6a6
raielin@gmail.com <raielin@gmail.com>
2014-11-05 09:28:46 -0500

    Wrote comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+    render json: @comments
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render json: @comment
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content, :submission_id, :comment_id)
+    end
+end
