498f539a681e04d1d64ac9cd49f15afb5cb4e451
raielin@gmail.com <raielin@gmail.com>
2014-11-05 09:59:09 -0500

    Cleaned up formatting of comment and submission models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
   belongs_to :submission
+
   has_many :votes
   has_many :comments
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,10 +1,11 @@
 class Submission < ActiveRecord::Base
   belongs_to :user
+
   has_many :comments
   has_many :votes
 
   def self.top_20
-    Post.all.limit(20)   # Not the ideal way to do this, but will work for now. Come back to this.
+    Submission.all.limit(20)   # Not the ideal way to do this, but will work for now. Come back to this.
   end
 
 end
