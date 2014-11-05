ba0a7448ef2932c8fa9d069d32ee325dc37d1ada
George <gbanis@gmail.com>
2014-11-04 23:01:55 -0500

    Added create submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -8,4 +8,16 @@ class SubmissionsController < ApplicationController
     @submission = Submission.find(params[:id])
     render json: @submission
   end
+
+  # post: /submissions?name=George&email=gbanis@gmail.com&about=Message
+  def create
+    @submission = Submission.create(submission_params)
+    render json: @submission
+  end
+
+
+  def submission_params
+    params.permit(:title, :url, :body, :user_id)
+  end
+
 end
