10b4aa66b1746b67e4f12013c06b30f8627f8fc5
fishermanswharff <fishermanswharff@github.com>
2014-11-04 22:27:00 -0500

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  get '/app' => 'submissions#index'
+  get '/' => redirect('https://github.com/fishermanswharff/wdi_7_rails_lab_hn_api')
+  get '/submissions/newest'
+
   resources :users, only: [:index, :show]
   resources :submissions, only: [:index, :show, :new, :create]
   resources :comments, only: [:index, :show, :new, :create]
   resources :votes, only: [:create]
-  get '/submissions/newest'
+  
 end
