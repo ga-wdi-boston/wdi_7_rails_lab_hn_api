ebd2602700190dca812d4e00ffea8d66a98c6bfa
raielin@gmail.com <raielin@gmail.com>
2014-11-05 10:33:07 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,13 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :users, only: [:show, :index]
+  # get '/' => redirect('https://github.com/fishermanswharff/wdi_7_rails_lab_hn_api')
+  # get '/submissions/newest'
+
+  resources :users, only: [:index, :show]
+  resources :submissions, only: [:index, :show, :new, :create]
+  resources :comments, only: [:index, :show, :create]
+  resources :votes, only: [:create]
 
   # Example resource route with options:
   #   resources :products do
