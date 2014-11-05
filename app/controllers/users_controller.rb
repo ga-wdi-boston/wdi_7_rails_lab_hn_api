9b626cb6129a5a0286247922eec91c959d266989
raielin@gmail.com <raielin@gmail.com>
2014-11-05 13:05:11 -0500

    Changed new to create in create action and added render to create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
+    render json: @user
   end
 
   def user_params
