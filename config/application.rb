a18a31622e1c42b0feb04c15479111098c6c7f24
fishermanswharff <fishermanswharff@github.com>
2014-11-05 00:41:37 -0500

    app.rb, prod.rb

--- config/application.rb
+++ config/application.rb
@@ -17,20 +17,10 @@ Bundler.require(*Rails.groups)
 module Wdi7RailsLabHnApi
   class Application < Rails::Application
 
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors, :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-
-        resource '/cors',
-          :headers => :any,
-          :methods => [:post],
-          :credentials => false,
-          :max_age => 0
-
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :options, :head],
-          :max_age => 0
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
