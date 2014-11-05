cc3ae90d4486942ebf2a2e06a966a329d44a2f79
raielin@gmail.com <raielin@gmail.com>
2014-11-05 17:34:21 -0500

    Wrote Backbone router with just a home route and function for now, including comments.

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,30 @@
 // run local web server - bash command: $ ruby -run -e httpd . -p 4000
 
-$.ajax({
-    // URL is where our requests should hit
-    url: 'http://localhost:3000/users',
-    type: 'GET'
-}).done(function(response) {
-    console.table(response);
+var Router = Backbone.Router.extend({
+    routes: {
+        '': 'home',
+        // Syntax here is:
+        // 'URLpath': 'functionName'
+        'home': 'home'
+    },
+
+    home: function() {
+        $.ajax({
+            url: 'http://localhost:3000/users',
+            type: 'GET'
+        }).done(function(response) {
+            console.table(response);
+        });
+    }
 });
+
+// Router doesn't do anything by iteslf.
+// Need to creates new Router instance in order
+// to run the functions in the object
+var router = new Router();
+
+// Tells Backbone to start tracking history
+// like reloading on a route, tracking backwards
+// and forwards buttons -- absolutely necessary
+// for it to work
+Backbone.history.start();
